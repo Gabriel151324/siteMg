df3ce602e6a28bb78b7ef9a850e962fc10f605d1
Gabriel <gabriel.aguiar.fidelis13@gmail.com>
2025-12-30 00:23:41 -0300

    Update 1.2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const links = document.querySelectorAll('.links a');
 function openMenu() {
     menu.classList.add('active');
     overlay.classList.add('active');
-    menuToggle.classList.add('hidde');
+    menuToggle.classList.add('hidden');
 }
 
 function closeMenu() {
